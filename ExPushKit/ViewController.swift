75729a24a9087289a041b7868ca91da33ab6807b
jake <palatable7@naver.com>
2022-02-10 23:54:35 +0900

    Processing..

--- ExPushKit/ViewController.swift
+++ ExPushKit/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import CallKit
+import PushKit
 
 class ViewController: UIViewController {
-
+  let provider = CXProvider(configuration: CXProviderConfiguration(localizedName: "jake"))
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    let registry = PKPushRegistry(queue: nil)
+    registry.delegate = self
+    registry.desiredPushTypes = [PKPushType.voIP]
   }
-
-
 }
 
+extension ViewController: CXProviderDelegate {
+  func providerDidReset(_ provider: CXProvider) {
+  }
+  
+  func provider(_ provider: CXProvider, perform action: CXAnswerCallAction) {
+    action.fulfill()
+  }
+  
+  func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
+    action.fulfill()
+  }
+}
+
+extension ViewController: PKPushRegistryDelegate {
+  func pushRegistry(
+    _ registry: PKPushRegistry,
+    didUpdate pushCredentials: PKPushCredentials,
+    for type: PKPushType
+  ) {
+    print(pushCredentials.token.map { String(format: "%02.2hhx", $0) }.joined())
+  }
+  
+  func pushRegistry(
+    _ registry: PKPushRegistry,
+    didReceiveIncomingPushWith payload: PKPushPayload,
+    for type: PKPushType, completion: @escaping () -> Void
+  ) {
+    self.provider.setDelegate(self, queue: nil)
+    let update = CXCallUpdate()
+    update.remoteHandle = CXHandle(type: .generic, value: "jake")
+    update.hasVideo = false
+    self.provider.reportNewIncomingCall(with: UUID(), update: update, completion: { error in })
+  }
+}
